7f0b25036f9980de4ae31573326c5a68cd872a2e
nasta <nastatimshina@gmail.com>
2024-02-28 22:21:02 +0300

    3rd task (split str by abc) and runner for this task

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose the task (1-2): ");
+        System.out.println("Choose the task (1-3): ");
         int choice = scanner.nextInt();
-        switch (choice){
-            case 1:
+        switch (choice) {
+            case 1 -> {
                 FirstTask changeStr = new FirstTask();
                 changeStr.changingText();
-                break;
-            case 2:
+            }
+            case 2 -> {
                 SecondTask str = new SecondTask();
                 str.concat();
-                break;
-            default:
-                System.out.println("Wrong option");
+            }
+            case 3 -> {
+                ThirdTask strABC = new ThirdTask();
+                strABC.findABC();
+            }
+            default -> System.out.println("Wrong option");
         }
 
     }
---
+++ src/main/java/ThirdTask.java
@@ -0,0 +1,20 @@
+/*Дана строка: teamwithsomeofexcersicesabcwanttomakeitbetter.
+
+Необходимо найти в данной строке "abc", записав всё
+что до этих символов в первую переменную, а также всё, что после них во вторую.
+
+Результат вывести в консоль. */
+public class ThirdTask {
+    void findABC(){
+        String str = "teamwithsomeofexcersicesabcwanttomakeitbetter";
+        int i = -1;
+        String s1;
+        String s2;
+
+        i = str.indexOf("abc");
+
+        s1 = str.substring(0, i);
+        s2 = str.substring(i+3);
+        System.out.printf("Your str: %s\nYour s1: %s\nYour s2: %s", str, s1, s2);
+    }
+}
