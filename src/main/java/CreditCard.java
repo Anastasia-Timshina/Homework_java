bf1d9387c1ca4330d6c7ad25f677c186e91e4a0f
nasta <nastatimshina@gmail.com>
2024-03-07 20:34:08 +0300

    class CreditCard with methods: "deposit", "withdraw", "printCardInfo" + runner for this task

---
+++ src/main/java/CreditCard.java
@@ -0,0 +1,39 @@
+/*2. Создать класс CreditCard c полями номер счета, текущая сумма на счету.
+Добавьте метод, который позволяет начислять сумму на кредитную
+карточку.
+Добавьте метод, который позволяет снимать с карточки некоторую
+сумму.
+Добавьте метод, который выводит текущую информацию о карточке.
+Напишите программу, которая создает три объекта класса CreditCard у
+которых заданы номер счета и начальная сумма
+Тестовый сценарий для проверки:
+Положите деньги на первые две карточки и снимите с третьей.
+Выведите на экран текущее состояние всех трех карточек.*/
+public class CreditCard {
+    private String accountNumber;
+    private double balance;
+
+    public CreditCard(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds :(");
+        }
+    }
+
+    public void printCardInfo() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: $" + balance);
+        System.out.println();
+    }
+
+}
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,14 +4,14 @@ public class Runner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Choose the task: (1)");
+        System.out.println("Choose the task: (1-2)");
         int choice = scanner.nextInt();
 
-        switch (choice){
-            case 1: {
-                Phone firstPhone = new Phone (12, "iphone12", 123);
-                Phone secondPhone = new Phone (23, "iphone13pro", 145);
-                Phone thirdPhone = new Phone (42, "iphone15promax", 180);
+        switch (choice) {
+            case 1 -> {
+                Phone firstPhone = new Phone(12, "iphone12", 123);
+                Phone secondPhone = new Phone(23, "iphone13pro", 145);
+                Phone thirdPhone = new Phone(42, "iphone15promax", 180);
 
                 System.out.println(firstPhone);
                 System.out.println(secondPhone);
@@ -22,7 +22,26 @@ public class Runner {
                 thirdPhone.receiveCall("Dasha", 87878787);
                 firstPhone.sendMessage(654738, 653290, 76239);
 
-                break;
+            }
+            case 2 -> {
+                CreditCard card1 = new CreditCard("1111111111111111", 0.0);
+                CreditCard card2 = new CreditCard("2222222222222222", 500.0);
+                CreditCard card3 = new CreditCard("3333333333333333", 1000.0);
+
+                card1.deposit(500.0);
+
+                System.out.println("Enter deposit for 2 card");
+                double amount = scanner.nextDouble();
+                card2.deposit(amount);
+                System.out.println("---------------------------------------");
+
+                card3.withdraw(1500.0);
+
+                System.out.println("---------------------------------------");
+                card1.printCardInfo();
+                card2.printCardInfo();
+                card3.printCardInfo();
+
             }
         }
     }
